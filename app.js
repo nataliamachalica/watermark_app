b023809f9fb78815db6c7c57087f28ef5f97f021
Natalia Szlapka <n.machalica@gmail.com>
2021-06-05 22:41:16 +0200

    Add logs

--- app.js
+++ app.js
@@ -1,30 +1,48 @@
 const Jimp = require('jimp');
 const inquirer = require('inquirer');
+const fs = require('fs');
 
 const addTextWatermarkToImage = async function(inputFile, outputFile, text) {
-    const image = await Jimp.read(inputFile);
-    const font = await Jimp.loadFont(Jimp.FONT_SANS_32_BLACK);
-	const textData = {
-		text,
-		alignmentX: Jimp.HORIZONTAL_ALIGN_CENTER,
-		alignmentY: Jimp.VERTICAL_ALIGN_MIDDLE,
-	};
 
-    image.print(font, 0, 0, textData, image.getWidth(), image.getHeight());
-    await image.quality(100).writeAsync(outputFile);
+	try {
+        const image = await Jimp.read(inputFile);
+        const font = await Jimp.loadFont(Jimp.FONT_SANS_32_BLACK);
+        const textData = {
+            text,
+            alignmentX: Jimp.HORIZONTAL_ALIGN_CENTER,
+            alignmentY: Jimp.VERTICAL_ALIGN_MIDDLE,
+        };
+
+        image.print(font, 0, 0, textData, image.getWidth(), image.getHeight());
+        await image.quality(100).writeAsync(outputFile);
+		console.log('Your file has been successfully created');
+
+	} catch {
+		console.log('Something went wrong. Your input is invalid');
+	}
+	startApp();
 };
 
 const addImageWatermarkToImage = async function(inputFile, outputFile, watermarkFile) {
-	const image = await Jimp.read(inputFile);
-	const watermark = await Jimp.read(watermarkFile);
-	const x = image.getWidth() / 2 - watermark.getWidth() / 2;
-	const y = image.getHeight() / 2 - watermark.getHeight() / 2;
 
-	image.composite(watermark, x, y, {
+	try {
+        const image = await Jimp.read(inputFile);
+        const watermark = await Jimp.read(watermarkFile);
+        const x = image.getWidth() / 2 - watermark.getWidth() / 2;
+        const y = image.getHeight() / 2 - watermark.getHeight() / 2;
+
+		image.composite(watermark, x, y, {
 		mode: Jimp.BLEND_SOURCE_OVER,
         opacitySource: 0.5,
-	});
-	await image.quality(100).writeAsync(outputFile);
+        });
+
+        await image.quality(100).writeAsync(outputFile);
+        console.log('Your file has been successfully created');
+
+    } catch {
+		console.log('Something went wrong. Your input is invalid');
+	}
+	startApp();
 };
 
 const prepareOutputFilename = (filename) => {
@@ -56,24 +74,36 @@ const startApp = async () => {
 	choices: ['Text watermark', 'Image watermark'],
 	}]);
 
+	const inputFilePath = './img/' + options.inputImage;
+
 	if(options.watermarkType === 'Text watermark') {
 		const text = await inquirer.prompt([{
-		name: 'value',
-		type: 'input',
-		message: 'Type your watermark text:',
+            name: 'value',
+            type: 'input',
+            message: 'Type your watermark text:',
 		}]);
 		options.watermarkText = text.value;
-		addTextWatermarkToImage('./img/' + options.inputImage, './img/' + prepareOutputFilename(options.inputImage), options.watermarkText);
-	}
-	else {
-		const image = await inquirer.prompt([{
-		name: 'filename',
-		type: 'input',
-		message: 'Type your watermark name:',
-		default: 'logo.png',
-		}]);
-		options.watermarkImage = image.filename;
-		addImageWatermarkToImage('./img/' + options.inputImage, './img/' + prepareOutputFilename(options.inputImage), './img/' + options.watermarkImage);
+
+		if (fs.existsSync(inputFilePath)){
+		addTextWatermarkToImage(inputFilePath, './img/' + prepareOutputFilename(options.inputImage), options.watermarkText);
+        } else
+			console.log('Something went wrong');
+		}
+
+        else {
+            const image = await inquirer.prompt([{
+            name: 'filename',
+            type: 'input',
+            message: 'Type your watermark name:',
+            default: 'logo.png',
+        }]);
+        options.watermarkImage = image.filename;
+
+		const watermarkToImage = './img/' + options.watermarkImage;
+
+		if (fs.existsSync(inputFilePath) && fs.existsSync(watermarkToImage)){
+			addImageWatermarkToImage(inputFilePath, './img/' + prepareOutputFilename(options.inputImage), watermarkToImage);
+		} else console.log('Something went wrong. Your input is invalid');
 	}
 };
 
